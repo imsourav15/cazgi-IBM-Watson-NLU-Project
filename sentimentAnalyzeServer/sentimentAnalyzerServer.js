2994a1cf04cbca378f31f4b848475fbcf3af0d03
imsourav15 <souravshandilya1507@gmail.com>
2021-06-26 12:50:56 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+        apikey: api_key,
+    }),
+    serviceUrl: api_url,
+    });
+    
+    const analyzeParams = {
+        'url': 'www.ibm.com',
+        'features': {
+            'categories': {
+            'limit': 3
+            }
+        }
+        };
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+        console.log('error:', err);
+    });
+    return naturalLanguageUnderstanding;
+}
+
 
 app.use(express.static('client'))
 
@@ -30,4 +66,3 @@ app.get("/text/sentiment", (req,res) => {
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
